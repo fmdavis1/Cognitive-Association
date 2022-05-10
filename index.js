de5e312a1b302615d131e07329b27079024b8e2f
Felecia Davis <fmdavis1@gmail.com>
2022-05-10 15:10:22 -0400

    updates for the css and html files and changes to the js file.

--- index.js
+++ index.js
@@ -3,24 +3,113 @@ let sequence = [];
 let humanSequence = []
 let level = 0;
 
-const startButton = document.querySelector('.js.start')
+const startButton = document.querySelector('.js-start')
+const info = document.querySelector('.js-info');
+const heading = document.querySelector('.js-heading');
+const tileContainer = document.querySelector('.js-container');
 
-function startGame (){
-    startButton.classList.add('hidden')
+function resetGame(text){
+    alert(text);
+    sequence = []
+    humanSequence = []
+    level = 0;
+    startButton.classList.remove('hidden');
+    heading.textContent = 'Cognitive Connection';
+    info.classList.add('hidden');
+    tileContainer.classList.add('unclickable');
+
 }
-startButton.addEventListener('click', startGame);
+function humanTurn(level){
+    tileContainer.classList.remove('unclickable');
+    info.textContent = `Your turn ${level} Tap${level > 1 ? 's': ''}`;
+}
 
-const info = document.querySelector('js.info');
+
+
+function activateTile(color){
+    const tile = document.querySelector(`[data-tile='${color}']`)
+    const sound = document.querySelector(`[data-sound='${color}']`)
+
+    tile.classList.add('activated');
+    sound.play();
+
+    setTimeout(() => {
+        tile.classList.remove('activated');
+    }, 300);
+    }
+
+    function playRound(nextSequence){
+        nextSequence.forEach ((color,index) => {
+            setTimeout(() => {
+                activateTile(color);
+
+            }, (index + 1)  * 600);
+        });
+    }
+
+function nextStep(){
+    const tiles = ['red', 'green', 'blue','yellow']
+    const random = tiles[Math.floor(Math.random() * tiles.length)]
+
+    return random;
+}
 
 function nextRound(){
     level += 1;
-    const nextSequece = sequence;
+
+    tileContainer.classList.add('unclickable');
+    info.textContent = "Wait for the computer"
+    heading.textContent = `Level ${level} of 20`; 
+    const nextSequence =[...sequence];
+    nextSequence.push(nextStep());
+    playRound(nextSequence);
+
+    sequence = [...nextSequence];
+    setTimeout (() => {
+        humanTurn(level);
+    }, level * 600 + 1000);
 
 
 }
 
-function startGame(){
-    startButton.classList.add('hidden');
+function handleClick(tile){
+    const index = humanSequence.push(tile) - 1;
+    const sound = document.querySelector(`[data-sound='${tile}']`);
+    sound.play;
+
+    const remainingTaps = sequence.length - humanSequence.length;
+
+    if (humanSequence[index]  !== sequence[index]){
+        resetGame('Oops! Game over, you pressed the wrong tile');
+        return;
+    }
+    if(humanSequence.length === sequence.length){
+        if (humanSequence.length === 10){
+            resetGame('Congrats! You completed all the levels');
+        }
+        humanSequence = [];
+        info.textContent = 'Succes! Keep going!';
+        setTimeout(() => {
+            nextRound();
+        }, 1000);
+        return;
+    }
+
+    info.textContent = `Your turn ${remainingTaps} Taps${remainingTaps > 1 ? 's' : ''}`;
+}
+function startGame (){
+    startButton.classList.add('hidden')
     info.classList.remove('hidden')
     info.textContent = "Wait for the computer";
+    nextRound();
+
 }
+startButton.addEventListener('click', startGame);
+
+tileContainer.addEventListener('click', (event) => {
+    const {tile} = event.target.dataset;
+    console.log(event)
+
+    if (tile) handleClick(tile)
+})
+
